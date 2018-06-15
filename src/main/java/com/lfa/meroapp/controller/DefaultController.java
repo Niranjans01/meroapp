f841fdfc26e814aeffbc2e3938bad53f0ad0fcda
Niranjans01 <nkrsubedi@gmail.com>
2018-06-15 14:55:27 +0545

    mero app first commit

--- src/main/java/com/lfa/meroapp/controller/DefaultController.java
+++ src/main/java/com/lfa/meroapp/controller/DefaultController.java
@@ -5,6 +5,13 @@
  */
 package com.lfa.meroapp.controller;
 
+import com.lfa.meroapp.entity.Departments;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +21,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/home")
 public class DefaultController {
     
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model){
+        String sql = "Select * from departments";
+        List<Departments> all = jdbcTemplate.query(sql, new RowMapper<Departments>() {
+            public Departments mapRow(ResultSet rs, int i) throws SQLException {
+                Departments department = new Departments();
+                department.setName(rs.getString("name"));
+                return department;
+            }
+        });
         model.addAttribute("user", "Niranjan");
+        model.addAttribute("department", all);
         return "index";
     }
     
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -4,6 +4,7 @@
     Author     : NiranjanKrSubedi
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -13,5 +14,12 @@
     </head>
     <body>
         <h1>Hello World! ${user}</h1>
+        <c:forEach var="d" items="${department}">
+        <ul>
+            <li>
+                ${d.name}
+            </li>
+        </ul>
+        </c:forEach>
     </body>
 </html>
