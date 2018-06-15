e6e8937eca0bb486db109a990950928d3720b75a
Niranjans01 <nkrsubedi@gmail.com>
2018-06-15 13:11:48 +0545

    mero app first commit

---
+++ src/main/java/com/lfa/meroapp/AppConfigurer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lfa.meroapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.lfa.meroapp")
+public class AppConfigurer  extends WebMvcConfigurerAdapter{
+    
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+       return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+    }
+    
+}
--- src/main/java/com/lfa/meroapp/controller/DefaultController.java
+++ src/main/java/com/lfa/meroapp/controller/DefaultController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(name = "/home")
+@RequestMapping(value = "/home")
 public class DefaultController {
     
     @RequestMapping(method = RequestMethod.GET)
